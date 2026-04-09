13707ddd84f21c045ab4aa881969f126d5949da6
Kyrylo Karasov <r1082844@student.thomasmore.be>
2026-04-09 20:23:29 +0200

    Restore board scene entity contract exports

--- docs/js/app/board-scene/contracts.js
+++ docs/js/app/board-scene/contracts.js
@@ -1,3 +1,14 @@
+export const BOARD_SCENE_ENTITY_KIND = Object.freeze({
+    edge: "edge",
+    node: "node",
+    space: "space"
+});
+
+export const BOARD_SCENE_POSITION_SPACE = Object.freeze({
+    local: "local",
+    pixel: "pixel"
+});
+
 export const BOARD_SCENE_PART_KIND = Object.freeze({
     connectionSession: "connection-session",
     dragSession: "drag-session",
